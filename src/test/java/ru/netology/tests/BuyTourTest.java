ade67c89daa00972f401a5a53bec423d3de77847
Constantin <zamotaev.post@gmail.com>
2021-04-18 16:05:05 +0400

    working version 1.1

--- src/test/java/ru/netology/tests/BuyTourTest.java
+++ src/test/java/ru/netology/tests/BuyTourTest.java
@@ -1,12 +1,13 @@
 package ru.netology.tests;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import ru.netology.data.Card;
 import ru.netology.data.DataHelper;
+import ru.netology.data.SqlHelper;
 import ru.netology.pages.FormField;
 import ru.netology.pages.Notification;
 import ru.netology.pages.PaymentType;
@@ -16,6 +17,7 @@ import static com.codeborne.selenide.Condition.*;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.data.DataHelper.getApprovedCard;
 import static ru.netology.data.DataHelper.openURL;
 import static ru.netology.pages.Notification.approvedPayment;
@@ -28,18 +30,34 @@ public class BuyTourTest {
         PaymentType.goToBuyTour();
     }
 
+    @AfterEach
+    public void cleanBase() {
+        SqlHelper.cleanDB();
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @Test
     void buyPositiveAllFieldValidApproved() {
         FormField form = new FormField();
         FormField.inputData(DataHelper.getApprovedCard());
         approvedPayment();
+        assertEquals("APPROVED", SqlHelper.getPaymentStatus());
     }
 
-    @Test
-    void buyPositiveAllFieldValidDeclined() {
-        FormField form = new FormField();
-        FormField.inputData(DataHelper.getDeclinedCard());
-        failedPayment();
-    }
+//    @Test
+//    void buyPositiveAllFieldValidDeclined() {
+//        FormField form = new FormField();
+//        FormField.inputData(DataHelper.getDeclinedCard());
+//        failedPayment();
+//    }
 
 }
